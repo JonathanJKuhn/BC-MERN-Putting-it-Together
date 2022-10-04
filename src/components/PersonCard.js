ca94e22897de595ff11b4842f67a7f7797b9b41a
Jonathan Kuhn <jkuhn96@gmail.com>
2022-10-04 01:11:32 -0400

    Added Birthday Button and functionality

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const PersonCard = (props) => {
-    const { fname,lname,age,hair } = props
+    const { fname,lname,age:initAge,hair } = props
+    const [age, setAge] = useState(initAge)
+    const incrementAge = (e) => {
+        setAge(age + 1)
+    }
     return (
         <div>
             <h2>{lname}, {fname}</h2>
             <p>Age: {age}</p>
             <p>Hair Color: {hair}</p>
+            <button onClick = { incrementAge }>Birthday Button for {fname} {lname}</button>
         </div>
     )
 }
